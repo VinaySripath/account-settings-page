dda8c803c0b205b268ba30ebd2b8d147ea4cc006
Vinay Sripath S <vinaysripath@gmail.com>
2023-01-11 12:48:53 +0530

    fix: show discription on setting to incomplete, add refresh button

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 import DoneIcon from "@mui/icons-material/Done";
 import PendingOutlinedIcon from "@mui/icons-material/PendingOutlined";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import ReplayIcon from "@mui/icons-material/Replay";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Settings = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const step = location.state.type;
+  console.log(step);
   const clickHandler = (status: string) => {
     navigate("/", { state: { status, step } });
   };
@@ -18,19 +20,38 @@ const Settings = () => {
         width: "100%",
         height: "100vh",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <IconButton onClick={() => clickHandler("completed")}>
-        <DoneIcon />
-      </IconButton>
-      <IconButton onClick={() => clickHandler("pending")}>
-        <PendingOutlinedIcon />
-      </IconButton>
-      <IconButton onClick={() => clickHandler("incomplete")}>
-        <CancelOutlinedIcon />
-      </IconButton>
+      <Box>
+        <IconButton sx={{ pr: 2 }} onClick={() => clickHandler("completed")}>
+          <Typography>Done</Typography>
+          <DoneIcon />
+        </IconButton>
+        <IconButton sx={{ pr: 2 }} onClick={() => clickHandler("pending")}>
+          <Typography>Pending</Typography>
+          <PendingOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box>
+        <IconButton sx={{ pr: 2 }} onClick={() => clickHandler("active")}>
+          <Typography>Failed</Typography>
+          <CancelOutlinedIcon />
+        </IconButton>
+        <IconButton
+          sx={{ pr: 2 }}
+          onClick={() => {
+            step === "email"
+              ? clickHandler("active")
+              : clickHandler("incomplete");
+          }}
+        >
+          <Typography>refresh</Typography>
+          <ReplayIcon />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
